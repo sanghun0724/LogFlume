add46aa93068f76ea5b57191b69435c9bf145766
= <sanghun0724@naver.com>
2023-04-24 21:02:02 +0900

    Initial Commit

---
+++ LogFlume/LogFlume.h
@@ -0,0 +1,18 @@
+//
+//  LogFlume.h
+//  LogFlume
+//
+//  Created by sangheon on 2023/04/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LogFlume.
+FOUNDATION_EXPORT double LogFlumeVersionNumber;
+
+//! Project version string for LogFlume.
+FOUNDATION_EXPORT const unsigned char LogFlumeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LogFlume/PublicHeader.h>
+
+
